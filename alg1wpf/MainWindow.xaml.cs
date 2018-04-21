b7ef39ef77dabcc5f1734952807b5f43846e396b
MorganRush <matvey159@gmail.com>
2018-04-21 20:24:15 +0300

    logical

--- alg1wpf/MainWindow.xaml.cs
+++ alg1wpf/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using alg1wpf.logical;
+using alg1wpf.view;
 
 namespace alg1wpf
 {
@@ -53,20 +43,66 @@ namespace alg1wpf
 
         private void ButtonResult_Click(object sender, RoutedEventArgs e)
         {
+            int numberExperiment = 0;
+            Algoritms algoritms = Algoritms.DijkstraDHeap;
             switch (SelectAlgoritm.Text)
             {
                 case "Алгоритм Дейкстры на основе 7-кучи":
                     {
+                        algoritms = Algoritms.DijkstraDHeap;
                         break;
                     }
                 case "Алгоритм Форда-Беллмана":
                     {
+                        algoritms = Algoritms.FordBellman;
                         break;
                     }
                 default:
                     {
                         return;
                     }
+            }
+            switch (SelectExperiment.Text)
+            {
+                case "1":
+                    {
+                        numberExperiment = 1;
+                        break;
+                    }
+                case "2":
+                    {
+                        numberExperiment = 2;
+                        break;
+                    }
+                default:
+                    {
+                        return;
+                    }
+            }
+            Graph graph = new Graph();
+            if (algoritms == Algoritms.DijkstraDHeap)
+            {
+                if (numberExperiment == 1)
+                {
+                    LineModel.ItemsSource = new PointModel(graph.FirstExperimentForDijkstraDHeap(), 100000, 100000).Points;
+                    LineModel.Visibility = Visibility.Visible;
+                }
+                else
+                {
+                    MessageBox.Show("1 2");
+                }
+            } 
+            else
+            {
+                if (numberExperiment == 1)
+                {
+                    MessageBox.Show("2 1");
+                }
+                else
+                {
+                    MessageBox.Show("2 2");
+                }
+
             }
         }
 
--- alg1wpf/view/PointModel.cs
+++ alg1wpf/view/PointModel.cs
@@ -6,13 +6,15 @@ namespace alg1wpf.view
 {
     public class PointModel
     {
-        public PointModel()
+        public PointModel(List<int> time, int minEdge, int stepEdge) 
         {
             Title = "Зависимость времени от кол-ва вершин";
             Points = new List<DataPoint>();
-            for (int i = 0; i < 100; i++)
+            int countEdge = minEdge;
+            for (int i = 0; i < time.Count; i++)
             {
-                Points.Add(new DataPoint(random.Next(10, 200), random.Next(100000, 10000000)));
+                Points.Add(new DataPoint(countEdge, time[i]));
+                countEdge += stepEdge;
             }
         }
         public Random random = new Random();
