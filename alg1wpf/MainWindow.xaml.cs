54512546bf4804ea4f28ca6f1145afda0aa29cd3
MorganRush <matvey159@gmail.com>
2018-04-21 19:39:09 +0300

    Добавьте файлы проекта.

---
+++ alg1wpf/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace alg1wpf
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            Window.Width = 850;
+            Window.Height = 450;
+            return;
+        }
+
+        private void SelectAlgoritm_DropDownClosed(object sender, EventArgs e)
+        {
+            if (SelectAlgoritm.Text == "")
+            {
+                return;
+            }
+            SelectExperiment.Visibility = Visibility.Visible;
+            TitleExperiment.Visibility = Visibility.Visible;
+        }
+
+        private void SelectExperiment_DropDownClosed(object sender, EventArgs e)
+        {
+            if (SelectAlgoritm.Text == "")
+            {
+                return;
+            }
+            ButtonResult.IsEnabled = true;
+        }
+
+        private void ButtonResult_Click(object sender, RoutedEventArgs e)
+        {
+            switch (SelectAlgoritm.Text)
+            {
+                case "Алгоритм Дейкстры на основе 7-кучи":
+                    {
+                        break;
+                    }
+                case "Алгоритм Форда-Беллмана":
+                    {
+                        break;
+                    }
+                default:
+                    {
+                        return;
+                    }
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ alg1wpf/view/PointModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using OxyPlot;
+
+namespace alg1wpf.view
+{
+    public class PointModel
+    {
+        public PointModel()
+        {
+            Title = "Зависимость времени от кол-ва вершин";
+            Points = new List<DataPoint>();
+            for (int i = 0; i < 100; i++)
+            {
+                Points.Add(new DataPoint(random.Next(10, 200), random.Next(100000, 10000000)));
+            }
+        }
+        public Random random = new Random();
+        public string Title { get; private set; }
+        public IList<DataPoint> Points { get; private set; }
+    }
+}
